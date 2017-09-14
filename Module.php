cabdfb8d393d0fd88545fae062b26118db572ce8
andrtechno <andrew.panix@gmail.com>
2017-09-14 15:26:50 +0300

    init

--- Module.php
+++ Module.php
@@ -7,24 +7,23 @@ use panix\engine\WebModule;
 
 class Module extends WebModule {
 
-    public $icon = 'icon-1c';
+    public $icon = '1c';
     public $routes = [
         'exchange1c/<password>' => 'exchange1c/default/index',
         'exchange1c/<password>/*' => 'exchange1c/default/index',
     ];
-
-    public function getAdminMenu2() {
-        return array(
-            'shop' => array(
-                'items' => array(
-                    array(
-                        'label' => $this->name,
-                        'url' => $this->adminHomeUrl,
-                        'icon' => Html::icon($this->icon),
-                    ),
-                ),
-            ),
-        );
+    public function getAdminMenu() {
+        return [
+            'shop' => [
+                'items' => [
+                    [
+                        'label' => Yii::t('exchange1c/default', 'MODULE_NAME'),
+                        'url' => ['/admin/exchange1c'],
+                          'icon' => $this->icon,
+                    ],
+                ],
+            ],
+        ];
     }
 
     public function getInfo() {
