d1b498897f335cde424a3dfb842bcbcf907a5127
Andrew <andrew.panix@gmail.com>
2019-10-02 16:43:49 +0300

    init

--- migrations/m170913_151246_exchange1c.php
+++ migrations/m170913_151246_exchange1c.php
@@ -1,29 +1,30 @@
 <?php
 
-use yii\db\Migration;
+namespace panix\mod\exchange1c\migrations;
+
+use panix\engine\db\Migration;
 
 /**
  * Class m170913_151246_exchange1c
  */
-class m170913_151246_exchange1c extends Migration {
+class m170913_151246_exchange1c extends Migration
+{
 
-    public function up() {
-        $tableOptions = null;
-        if ($this->db->driverName === 'mysql') {
-            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
-        }
+    public function up()
+    {
         $this->createTable('{{%exchange1c}}', [
-            'id' => $this->primaryKey(),
-            'object_id' => $this->primaryKey(),
-            'object_type' => $this->integer(),
+            'id' => $this->primaryKey()->unsigned(),
+            'object_id' => $this->integer()->unsigned(),
+            'object_type' => $this->tinyInteger(2)->unsigned(),
             'external_id' => $this->string(255),
-                ], $tableOptions);
+        ], $this->tableOptions);
 
         $this->createIndex('external_id', '{{%exchange1c}}', 'external_id', 0);
         $this->createIndex('object_type', '{{%exchange1c}}', 'object_type', 0);
     }
 
-    public function down() {
+    public function down()
+    {
         $this->dropTable('{{%exchange1c}}');
     }
 
